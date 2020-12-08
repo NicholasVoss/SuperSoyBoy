84f6d55c7fa7f4b57e92bc948f1f903540551acb
NicholasVoss <envy_456@yahoo.ca>
2020-12-08 16:22:09 -0500

    Chapter 14 - added time leaderboard

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public string playerName;
     public static GameManager instance;
+
+    void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
 
     void Awake()
     {
@@ -30,5 +40,64 @@ public class GameManager : MonoBehaviour
     {
         yield return new WaitForSeconds(delay);
         SceneManager.LoadScene("Game");
+    }
+
+    public List<PlayerTimeEntry> LoadPreviousTimes()
+    {
+        try
+        {
+            var scoresFile = Application.persistentDataPath + "/" + playerName + "_times.dat";
+            using (var stream = File.Open(scoresFile, FileMode.Open))
+            {
+                var bin = new BinaryFormatter();
+                var times = (List<PlayerTimeEntry>)bin.Deserialize(stream);
+                return times;
+            }
+        }
+
+        catch (IOException ex)
+        {
+            Debug.LogWarning("Couldn't load previous times for: " + playerName + ". Exception: " + ex.Message);
+            return new List<PlayerTimeEntry>();
+        }
+    }
+
+    public void SaveTime(decimal time)
+    {
+        var times = LoadPreviousTimes();
+        
+        var newTime = new PlayerTimeEntry();
+        newTime.entryDate = DateTime.Now;
+        newTime.time = time;
+        
+        var bFormatter = new BinaryFormatter();
+        var filePath = Application.persistentDataPath + "/" + playerName + "_times.dat";
+        using (var file = File.Open(filePath, FileMode.Create))
+        {
+            times.Add(newTime);
+            bFormatter.Serialize(file, times);
+        }
+    }
+
+    public void DisplayPreviousTimes()
+    {
+        var times = LoadPreviousTimes();
+        var topThree = times.OrderBy(time => time.time).Take(3);
+        
+        var timesLabel = GameObject.Find("PreviousTimes").GetComponent<Text>();
+        
+        timesLabel.text = "BEST TIMES \n";
+        foreach (var time in topThree)
+        {
+            timesLabel.text += time.entryDate.ToShortDateString() + ": " + time.time + "\n";
+        }
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode loadscenemode)
+    {
+        if (scene.name == "Game")
+        {
+            DisplayPreviousTimes();
+        }
     }
 }
